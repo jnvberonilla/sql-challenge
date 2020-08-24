34c210d431cdf7adfcd1068f86e67c065bb642c2
jnvberonilla <66648401+jnvberonilla@users.noreply.github.com>
2020-08-23 22:48:14 -0700

    Add files via upload

---
+++ queries copy.sql
@@ -0,0 +1,64 @@
+-- 1
+
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.sex,
+Salaries.salary
+FROM Employees
+INNER JOIN Salaries ON
+Employees.emp_no = Salaries.emp_no;
+
+--2
+SELECT emp_no, first_name, last_name, hire_date from Employees
+WHERE hire_date >= '1985-12-31'
+AND hire_date < '1987-01-01';
+
+--3
+SELECT Department_Manager.dept_no, 
+	   Departments.dept_name,
+	   Department_Manager.emp_no,
+	   Employees.last_name,
+	   Employees.first_name 
+FROM Department_Manager
+INNER JOIN Departments ON
+Department_Manager.dept_no = Departments.dept_no
+INNER JOIN Employees ON
+Department_Manager.emp_no = Employees.emp_no;
+
+--4
+SELECT Employees.emp_no, 
+	   Employees.last_name, 
+	   Employees.first_name,
+	   Departments.dept_name
+FROM Employees
+INNER JOIN Department_Manager ON
+Employees.emp_no = Department_Manager.emp_no
+INNER JOIN Departments ON
+Department_Manager.dept_no = Departments.dept_no;
+
+--5
+SELECT * FROM Employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--6
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Employees AS e
+JOIN Department_Employees ON
+e.emp_no = Department_Employees.emp_no
+INNER JOIN Departments AS d ON
+Department_Employees.dept_no = d.dept_no
+WHERE dept_name = 'Sales';
+
+--7
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Employees AS e
+JOIN Department_Employees ON
+e.emp_no = Department_Employees.emp_no
+INNER JOIN Departments AS d ON
+Department_Employees.dept_no = d.dept_no
+WHERE dept_name = 'Sales' OR 
+	  dept_name = 'Development';
+
+--8
+SELECT last_name, COUNT(last_name) FROM Employees
+GROUP BY last_name
+ORDER BY count(last_name) desc;
